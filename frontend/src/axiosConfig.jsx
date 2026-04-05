d36d63a4aaf49e9267057f7e419dda5a48cd9f53
Arjun Ror <n11681365@qut.edu.au>
2026-04-05 23:20:52 +1000

    Update API URL to yesterday EC2 IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.106.188.184', // live EC2 via Nginx
+  baseURL: 'http://16.176.219.14', // live EC2 via Nginx
   headers: { 'Content-Type': 'application/json' },
 });
 
